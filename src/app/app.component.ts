00451f8170a51bb498b4942d0f451d75437a5653
Stephen Bolivar <bolivar.stephen@gmail.com>
2018-11-02 13:44:41 -0400

    fullscree map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gmaps-routing-demo';
+  lat = 16.2650;
+  lng = -61.5710;
+  zoom = 12;
+  map: google.maps.Map | undefined = undefined;
+
+
+  onMapReady(map: google.maps.Map) {
+    this.map = map;
+  }
 }
