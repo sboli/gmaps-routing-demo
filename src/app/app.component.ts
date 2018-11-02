2cb521e8fa3f57f7feba8d3ad65bd27632cf0db9
Stephen Bolivar <bolivar.stephen@gmail.com>
2018-11-02 13:55:55 -0400

    dropping markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { MouseEvent, LatLngLiteral } from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gmaps-routing-demo';
-  lat = 16.2650;
-  lng = -61.5710;
+  guadeloupe = {
+    lat: 16.2650,
+    lng: -61.5710
+  };
   zoom = 12;
   map: google.maps.Map | undefined = undefined;
+  markers = [];
 
+  ngOnInit(): void {
+  }
 
   onMapReady(map: google.maps.Map) {
     this.map = map;
+  }
+
+  onMapClick(evt: MouseEvent) {
+    this.addMarker(evt.coords);
+  }
+
+  addMarker(latLng: LatLngLiteral) {
+    this.markers.push(latLng);
   }
 }
